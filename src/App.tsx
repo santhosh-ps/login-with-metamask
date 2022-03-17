d37c47d0f603720074bc054c0fd518414e67b84d
santhosh-ps <pssanthosh13@gmail.com>
2022-03-17 09:22:54 +0530

    quick login with metamask example
    Ethersjs package used to wrap web3
    sign nonce, verify signature
    and get back token.
    some styles updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { ethers } from "ethers";
 
 function App() {
+  const [isMetamaskInstalled, setIsMetamaskInstalled] = useState(
+    !!(window as any).ethereum
+  );
+  const storedToken = localStorage.getItem('token');
+  const [token, setToken] = useState(storedToken);
+  const onLogin = async() => {
+    const provider = new ethers.providers.Web3Provider((window as any).ethereum)
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner()
+    const walletAddress = await signer.getAddress();
+    const nonceResponse = await fetch('https://rapid-striped-advantage.glitch.me/api/metamask/login', {
+      method: 'Post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({walletAddress})
+    });
+    const {nonce} = await nonceResponse.json();
+    const signature = await signer.signMessage(nonce);
+    const loginResponse = await fetch('https://rapid-striped-advantage.glitch.me/api/metamask/verify', {
+      method: 'Post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({walletAddress, signature})
+    });
+    const {token} = await loginResponse.json();
+    localStorage.setItem('token', token);
+    setToken(token);
+  }
+  const onLogout = () => {
+    localStorage.clear();
+    setToken(null);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='login-btn-wrap'>
+        <button hidden={!!token} disabled={!isMetamaskInstalled} onClick={onLogin}>
+            Login With Metamask
+        </button>
+        {
+          token && <>
+                      <div>Successfully Logged in With Metamask!. JWT: </div>
+                      <div className='token-wrap'><code style={{width: '120px'}}>{token}</code></div>
+                      <div><button onClick={onLogout}>Logout</button></div>
+                  </>
+
+        }
+      </div>
     </div>
   );
 }
